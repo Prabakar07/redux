3549c5161f521d43e162693960ba63455aa75e7f
Prabakar07 <prabakarsv7@gmail.com>
2024-02-12 17:50:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Counter } from './Components/Counter';
+
+function App() {
+  return (
+    <>
+    <Counter/>
+    </>
+  );
+}
+
+export default App;
